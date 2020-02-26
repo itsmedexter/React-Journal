21041fba71d44329ce01e8a78e547f1c439e8438
Dexter Valencia <deeside@pacbell.net>
2020-02-26 14:22:00 -0800

    updated login route

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,15 +4,20 @@ module.exports = {
     login: function(req, res) {
         // handle user login
         db.User.find(req.body)
+        .then(dbUser => {
+            res.json (dbUser)
+            // if user doesn't exit
+        })
         .catch(err => res.status (400).json(err));
     },
 
     findAll: function(req, res) {
-        db.User.find(req.query)
+        db.User.find({})
         .sort({ date: -1 })
         .then(dbUser => res.json (dbUser))
         .catch(err => res.status (400).json(err));
     },
+    // This is it!
     findById: async function(req, res) {
         try {
         const dbUser = await db.User.findById(req.params.id)
